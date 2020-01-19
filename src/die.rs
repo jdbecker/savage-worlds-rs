93ca58205648f27674134bd240c57a8092aec799
jdbecker <jdbecker@users.noreply.github.com>
2020-01-19 16:07:51 -0600

    re-implement Die as enum

--- src/die.rs
+++ src/die.rs
@@ -1,41 +1,27 @@
 use rand::Rng;
 
-pub struct Die {
-    min: u8,
-    max: u8,
+#[derive(PartialEq, Debug)]
+pub enum Die {
+    d4,
+    d6,
+    d8,
+    d10,
+    d12,
 }
 
 impl Die {
-    pub fn new(min: u8, max: u8) -> Die {
-        Die { min, max }
-    }
-
-    pub fn dx(max: u8) -> Die {
-        Die::new(1, max)
-    }
-
-    pub fn d4() -> Die {
-        Die::dx(4)
-    }
-
-    pub fn d6() -> Die {
-        Die::dx(6)
-    }
-
-    pub fn d8() -> Die {
-        Die::dx(8)
-    }
-
-    pub fn d10() -> Die {
-        Die::dx(10)
-    }
-
-    pub fn d12() -> Die {
-        Die::dx(12)
+    fn max(&self) -> u8 {
+        match self {
+            Die::d4 => 4,
+            Die::d6 => 6,
+            Die::d8 => 8,
+            Die::d10 => 10,
+            Die::d12 => 12,
+        }
     }
 
     pub fn roll(&self) -> u8 {
-        rand::thread_rng().gen_range(self.min, self.max + 1)
+        rand::thread_rng().gen_range(1, self.max() + 1)
     }
 }
 
@@ -45,14 +31,14 @@ mod tests {
 
     fn roll(d: &Die) -> u8 {
         let r = d.roll();
-        assert!(r >= d.min, "die roll was {}", r);
-        assert!(r <= d.max, "die roll was {}", r);
+        assert!(r >= 1, "die roll was {}", r);
+        assert!(r <= d.max(), "die roll was {}", r);
         r
     }
 
     #[test]
     fn can_roll_1_on_dx() {
-        let d = Die::dx(2);
+        let d = Die::d4;
         let mut r = roll(&d);
         while r != 1 {
             r = roll(&d);
@@ -61,7 +47,7 @@ mod tests {
 
     #[test]
     fn can_roll_4_on_d4() {
-        let d = Die::d4();
+        let d = Die::d4;
         let mut r = roll(&d);
         while r != 4 {
             r = roll(&d);
@@ -70,7 +56,7 @@ mod tests {
 
     #[test]
     fn can_roll_6_on_d6() {
-        let d = Die::d6();
+        let d = Die::d6;
         let mut r = roll(&d);
         while r != 6 {
             r = roll(&d);
@@ -79,7 +65,7 @@ mod tests {
 
     #[test]
     fn can_roll_8_on_d8() {
-        let d = Die::d8();
+        let d = Die::d8;
         let mut r = roll(&d);
         while r != 8 {
             r = roll(&d);
@@ -88,7 +74,7 @@ mod tests {
 
     #[test]
     fn can_roll_10_on_d10() {
-        let d = Die::d10();
+        let d = Die::d10;
         let mut r = roll(&d);
         while r != 10 {
             r = roll(&d);
@@ -97,7 +83,7 @@ mod tests {
 
     #[test]
     fn can_roll_12_on_d12() {
-        let d = Die::d12();
+        let d = Die::d12;
         let mut r = roll(&d);
         while r != 12 {
             r = roll(&d);
