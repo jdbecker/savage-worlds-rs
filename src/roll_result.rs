5962ff9b2694c1efdf24acc50c054ddd6b9955d4
jdbecker <jdbecker@users.noreply.github.com>
2020-01-21 18:51:40 -0600

    Add a couple tests to demonstrate RollResults functionality

--- src/roll_result.rs
+++ src/roll_result.rs
@@ -13,3 +13,25 @@ impl RollResult {
         self.dice.iter().map(|d| d.1).max().unwrap_or(0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::die::Die::{d4, d6};
+
+    #[test]
+    fn calc_total() {
+        let r = RollResult {
+            dice: vec![(d4, 3), (d6, 5)],
+        };
+        assert_eq!(r.total(), 8);
+    }
+
+    #[test]
+    fn calc_max() {
+        let r = RollResult {
+            dice: vec![(d4, 3), (d6, 5)],
+        };
+        assert_eq!(r.max(), 5);
+    }
+}
