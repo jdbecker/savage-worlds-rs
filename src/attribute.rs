86271106296dceb83916ede770491ef963031c69
jdbecker <jdbecker@users.noreply.github.com>
2020-02-03 07:55:59 -0600

    Implement roll attribute for extra (no wild die). Wildcard attribute roll to follow later.

--- src/attribute.rs
+++ src/attribute.rs
@@ -3,14 +3,18 @@ use crate::die::Die;
 
 pub struct Attribute {
     die: Die,
-    modifier: i8,
+    modifier: u8,
 }
 
 impl Attribute {
-    pub fn new() -> Attribute {
-        Attribute {
-            die: Die::d4,
-            modifier: 0,
+    pub fn new(die: Die, modifier: u8) -> Attribute {
+        Attribute { die, modifier }
+    }
+
+    pub fn roll(&self) -> DiceResult {
+        DiceResult {
+            results: vec![self.die.roll_explode()],
+            modifier: self.modifier,
         }
     }
 }
@@ -18,4 +22,21 @@ impl Attribute {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use regex::Regex;
+
+    #[test]
+    fn roll_simple_attr() {
+        let a = Attribute::new(Die::d4, 0);
+        assert!(Regex::new(r"^\d+ = \[d4:\d+(\([\d,]+\))?]$")
+            .unwrap()
+            .is_match(&a.roll().summarize_max()))
+    }
+
+    #[test]
+    fn roll_attr_modifier() {
+        let a = Attribute::new(Die::d4, 2);
+        assert!(Regex::new(r"^\d+ = 2 \+ \[d4:\d+(\([\d,]+\))?]$")
+            .unwrap()
+            .is_match(&a.roll().summarize_max()))
+    }
 }
