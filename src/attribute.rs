885ae665ed67fb949fc5fa1b64fb711874b06fc8
jdbecker <jdbecker@users.noreply.github.com>
2020-02-03 20:49:09 -0600

    Enable Attributes to roll wild die for wildcards

--- src/attribute.rs
+++ src/attribute.rs
@@ -13,12 +13,20 @@ impl Attribute {
             modifier: self.modifier,
         }
     }
+
+    pub fn roll_wildcard(&self) -> DiceResult {
+        DiceResult {
+            results: vec![self.die.roll_explode(), Die::d6.roll_explode()],
+            modifier: self.modifier,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use regex::Regex;
+
+    use super::*;
 
     #[test]
     fn roll_simple_attr() {
@@ -40,5 +48,31 @@ mod tests {
         assert!(Regex::new(r"^\d+ = 2 \+ \[d4:\d+(\([\d,]+\))?]$")
             .unwrap()
             .is_match(&a.roll().summarize_max()))
+    }
+
+    #[test]
+    fn roll_wildcard() {
+        let a = Attribute {
+            die: Die::d4,
+            modifier: 0,
+        };
+        assert!(
+            Regex::new(r"^\d+ = \[d4:\d+(\([\d,]+\))?], \[d6:\d+(\([\d,]+\))?]$")
+                .unwrap()
+                .is_match(&a.roll_wildcard().summarize_max())
+        )
+    }
+
+    #[test]
+    fn roll_wildcard_modifier() {
+        let a = Attribute {
+            die: Die::d4,
+            modifier: 1,
+        };
+        assert!(
+            Regex::new(r"^\d+ = 1 \+ \[d4:\d+(\([\d,]+\))?], \[d6:\d+(\([\d,]+\))?]$")
+                .unwrap()
+                .is_match(&a.roll_wildcard().summarize_max())
+        )
     }
 }
