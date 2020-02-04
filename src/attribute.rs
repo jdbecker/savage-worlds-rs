75e99d2a2ec3b6544adcff7c680a1d3d86f48a6e
jdbecker <jdbecker@users.noreply.github.com>
2020-02-03 20:15:57 -0600

    Remove unnecessary constructor from Attribute

--- src/attribute.rs
+++ src/attribute.rs
@@ -7,10 +7,6 @@ pub struct Attribute {
 }
 
 impl Attribute {
-    pub fn new(die: Die, modifier: u8) -> Attribute {
-        Attribute { die, modifier }
-    }
-
     pub fn roll(&self) -> DiceResult {
         DiceResult {
             results: vec![self.die.roll_explode()],
@@ -26,7 +22,10 @@ mod tests {
 
     #[test]
     fn roll_simple_attr() {
-        let a = Attribute::new(Die::d4, 0);
+        let a = Attribute {
+            die: Die::d4,
+            modifier: 0,
+        };
         assert!(Regex::new(r"^\d+ = \[d4:\d+(\([\d,]+\))?]$")
             .unwrap()
             .is_match(&a.roll().summarize_max()))
@@ -34,7 +33,10 @@ mod tests {
 
     #[test]
     fn roll_attr_modifier() {
-        let a = Attribute::new(Die::d4, 2);
+        let a = Attribute {
+            die: Die::d4,
+            modifier: 2,
+        };
         assert!(Regex::new(r"^\d+ = 2 \+ \[d4:\d+(\([\d,]+\))?]$")
             .unwrap()
             .is_match(&a.roll().summarize_max()))
