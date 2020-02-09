1ff556ecbd0921e7f80fe88ef52478e5cb68dca0
jdbecker <jdbecker@users.noreply.github.com>
2020-02-09 09:58:06 -0600

    add enum for attribute types

---
+++ src/character/attribute_types.rs
@@ -0,0 +1,7 @@
+pub enum AttributeTypes {
+    Agility,
+    Smarts,
+    Spirit,
+    Strength,
+    Vigor,
+}
--- src/character/mod.rs
+++ src/character/mod.rs
@@ -1,6 +1,7 @@
 use crate::character::attributes::Attributes;
 use crate::character::skills::Skills;
 
+mod attribute_types;
 mod attributes;
 mod skills;
 
