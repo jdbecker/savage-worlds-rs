65c0ee6b9a98a099ddb34511459bb7ca229bd1f4
jdbecker <jdbecker@users.noreply.github.com>
2020-02-09 12:02:23 -0600

    publicize character fields

--- src/character/mod.rs
+++ src/character/mod.rs
@@ -1,4 +1,5 @@
 use crate::character::attributes::Attributes;
+use crate::character::rank::Rank;
 use crate::character::skills::Skills;
 
 mod attribute;
@@ -10,8 +11,9 @@ mod skill;
 mod skills;
 
 pub struct Character {
-    attributes: Attributes,
-    skills: Skills,
+    pub rank: Rank,
+    pub attributes: Attributes,
+    pub skills: Skills,
 }
 
 impl Character {}
