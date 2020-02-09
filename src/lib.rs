b5e5e5cc50b08ea68324a2bf073d5f041befa0a8
jdbecker <jdbecker@users.noreply.github.com>
2020-02-09 08:54:49 -0600

    Add ranks enum

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod character_trait;
 pub mod dice_result;
 pub mod die;
 pub mod die_result;
+pub mod rank;
 pub mod skill;
 
 #[cfg(test)]
---
+++ src/rank.rs
@@ -0,0 +1,7 @@
+pub enum Rank {
+    Novice,
+    Seasoned,
+    Veteran,
+    Heroic,
+    Legendary,
+}
