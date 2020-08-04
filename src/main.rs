3e93bfe1722092363f90f55f28aea9054d296ad8
jbecker44 <jbecker@project44.com>
2020-08-04 15:32:32 -0500

    Add swagger docs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, routes};
+use rocket::get;
+use rocket_contrib::json::Json;
+use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+use rocket_okapi::{openapi, routes_with_openapi, JsonSchema};
 
+#[derive(serde::Serialize, JsonSchema)]
+struct Response {
+    reply: String,
+}
+
+#[openapi]
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn my_controller() -> Json<Response> {
+    Json(Response {
+        reply: "show me the docs!".to_string(),
+    })
+}
+
+fn get_docs() -> SwaggerUIConfig {
+    use rocket_okapi::swagger_ui::UrlObject;
+
+    SwaggerUIConfig {
+        url: "/my_resource/openapi.json".to_string(),
+        ..Default::default()
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/my_resource", routes_with_openapi![my_controller])
+        .mount("/swagger", make_swagger_ui(&get_docs()))
+        .launch();
 }
